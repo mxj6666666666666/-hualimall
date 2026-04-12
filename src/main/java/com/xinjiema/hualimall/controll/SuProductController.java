f16d4c0159b38347051e244b5efeb2c0330cd550
xinjiema <1143674039@qq.com>
2026-04-12 16:16:13 +0800

    已实现批量增加，修改，批量修改，删除，批量商品的基本功能

--- src/main/java/com/xinjiema/hualimall/controll/SuProductController.java
+++ src/main/java/com/xinjiema/hualimall/controll/SuProductController.java
@@ -5,10 +5,11 @@ import com.xinjiema.hualimall.pojo.Result;
 import com.xinjiema.hualimall.service.ProductService;
 import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.annotations.Delete;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -20,11 +21,44 @@ public class SuProductController {
 
     @PostMapping
     public Result<String> addProduct(Product product) {
-
         log.info("添加商品，参数：{}", product);
         productService.addProduct(product);
-
         return Result.success("添加商品成功");
     }
 
+    @PostMapping("/batch")
+    public Result<String> addBatch(@RequestBody List<Product> products) {
+        log.info("批量添加商品，参数：{}", products);
+        productService.addBatch(products);
+        return Result.success("批量添加商品成功");
+    }
+
+    @PutMapping("/{id}")
+    public Result<String> updateProduct(@PathVariable Long id, Product product) {
+        log.info("修改 id = {} 的商品，参数：{}", id, product);
+        product.setId(id);
+        productService.updateProduct(product);
+        return Result.success("修改商品成功");
+    }
+
+    @PutMapping
+    public Result<String> updateBatch(@RequestBody List<Product> products) {
+        log.info("批量修改商品，参数：{}", products);
+        productService.updateBatch(products);
+        return Result.success("批量修改商品成功");
+    }
+
+    @DeleteMapping("/{id}")
+    public Result<String> deleteProduct(@PathVariable Long id) {
+        log.info("删除 id = {} 的商品", id);
+        productService.deleteProduct(id);
+        return Result.success("删除商品成功");
+    }
+
+    @DeleteMapping
+    public Result<String> deleteBatch(@RequestBody List<Long> ids) {
+        log.info("批量删除商品，参数：{}", ids);
+        productService.deleteBatch(ids);
+        return Result.success("批量删除商品成功");
+    }
 }
--- src/main/java/com/xinjiema/hualimall/service/ProductService.java
+++ src/main/java/com/xinjiema/hualimall/service/ProductService.java
@@ -4,9 +4,21 @@ import com.xinjiema.hualimall.pojo.PageResult;
 import com.xinjiema.hualimall.pojo.ProQueryParams;
 import com.xinjiema.hualimall.pojo.Product;
 
+import java.util.List;
+
 public interface ProductService {
 
     PageResult<Product> findall(ProQueryParams proQueryParams);
 
     void addProduct(Product product);
+
+    void updateProduct(Product product);
+
+    void updateBatch(List<Product> product);
+
+    void deleteProduct(Long id);
+
+    void deleteBatch(List<Long> ids);
+
+    void addBatch(List<Product> products);
 }
